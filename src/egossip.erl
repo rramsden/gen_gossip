adfd96f43e2baa4fb640ddf196da9f849adf2730
Richard Ramsden <richard@rramsden.ca>
2012-10-30 11:35:58 -0700

    Register handler sounds better :)

--- src/egossip.erl
+++ src/egossip.erl
@@ -1,19 +1,10 @@
 -module(egossip).
--export([new/1, new/2]).
-
--define(HANDLER(Module), list_to_atom("egossip_" ++ atom_to_list(Module))).
--type opts() :: [sync].
+-export([register_handler/1]).
 
 %% @doc
 %% Spawn a new gossip handler
 %% @end
+-spec register_handler(module()) -> {ok, pid()}.
 
--spec new(module()) -> {ok, pid()}.
-
-new(Module) ->
-    new(Module, []).
-
--spec new(module(), opts()) -> {ok, pid()}.
-
-new(Module, Opts) ->
-    egossip_sup:start_child(Module, Opts).
+register_handler(Module) ->
+    egossip_sup:start_child(Module).
